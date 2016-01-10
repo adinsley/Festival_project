5ed6acb28bb79c7f066e3ec2b2c1949079cdd270
User <user@Users-MBP-7.home>
2016-01-10 20:48:59 +0000

    Sunday Night Commit

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -17,8 +17,10 @@ class BookingsController < ApplicationController
   end
 
   def index
+    @bookings = current_user.bookings
   end
 
   def show
+    @booking = Booking.find(params[:id])
   end
 end

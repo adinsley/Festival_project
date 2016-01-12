d400bda49e30948ae7d9ea8bea9bb5dcbe151702
User <user@Users-MacBook-Pro-7.local>
2016-01-12 18:03:55 +0000

    further work on views

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,8 +1,12 @@
 class VenuesController < ApplicationController
+  
   def new
+    @venue = Venue.new
   end
 
   def create
+    Venue.create(venue_params)
+    redirect_to(venues_path)
   end
 
   def update
@@ -26,4 +30,13 @@ class VenuesController < ApplicationController
   def show
     @venue = Venue.find(params[:id])
   end
+
+private
+
+def venue_params
+     params.require(:venue).permit(:name, :image, :description, :loaction, :link, :capacity)
+end
+
+
+
 end

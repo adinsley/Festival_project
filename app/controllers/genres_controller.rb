009be4724cc2ba286ac9bcf539458b3e30bb61fb
User <user@Users-MBP-7.home>
2016-01-12 22:14:15 +0000

    all views including genre working

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -4,12 +4,27 @@ class GenresController < ApplicationController
     @genre =Genre.new
   end
 
+  def create
+    Genre.create(genre_params)
+    redirect_to(genres_path)
+  end
+
   def index
       if params[:genre_id]
             @genres = Genre.search(params[:genre_id]).order("created_at DESC")
           else
             @genres = Genre.all.order("created_at DESC")
           end
-    end
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  private
+
+  def genre_params
+       params.require(:genre).permit(:name)
+  end
 
 end

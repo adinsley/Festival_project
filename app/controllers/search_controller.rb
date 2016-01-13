6f74081bbad7bcc9bcaaa488f5d727ce31d96980
User <user@Users-MBP-7.home>
2016-01-13 23:31:23 +0000

    searched fixed

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,10 @@
 class SearchController < ApplicationController
     def index
-        performances = Performance.search(params[:search])
+        
         venues = Venue.search(params[:search])
         shows = Show.search(params[:search])
-
-        @results = performances + venues + shows       
+       
+        @results = venues + shows       
       end
 
 end
--- app/models/performance.rb
+++ app/models/performance.rb
@@ -13,7 +13,7 @@ class Performance < ActiveRecord::Base
 
   def self.search(query)
       # where(:title, query) -> This would return an exact match of the query
-      where("start", "%#{query}%") 
+      where("description", "%#{query}%") 
     end
 
   def human_readable_start_date

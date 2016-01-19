3674066579e8a667ff21de8ce5bca29c7a75489d
User <user@Users-MacBook-Pro-7.local>
2016-01-19 14:28:59 +0000

    before cancan

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,16 @@
 class HomeController < ApplicationController
+
+  before_action :authenticate_user!, only: [:admin]
+
     def index
         
         @performances = Performance.newest_performances  
     end
 
     def admin
-      @venues = Venue.all
-      @shows = Show.all
-      @genres = Genre.all
+      @venues = Venue.all.order("name")
+      @shows = Show.all.order("title")
+      @genres = Genre.all.order("name")
       @performances = Performance.all.joins(:show).order("shows.title")
     end
 
